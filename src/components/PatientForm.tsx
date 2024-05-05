4f821b3024e13479ca8f0760e644c2cab79c3475
Franco Silva <franco_tigres18@hotmail.com>
2024-05-05 16:02:01 -0600

    feat(Toast Guardar paciente)

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import Error from './Error';
 import type { DraftPatient } from '../types';
 import { usePatientStore } from '../store';
@@ -38,6 +39,7 @@ export default function PatientForm() {
       updatePatient(data);
     } else {
       addPatient(data);
+      toast.success('Paciente registrado correctamente');
     }
 
     reset();
